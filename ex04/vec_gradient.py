f755ffbde9b4a8fc987e4265b958a3c2a46d7008
Florian Muller <fmuller@student.42.fr>
2020-06-08 17:58:35 +0200

    (docstring indentation)

--- ex04/vec_gradient.py
+++ ex04/vec_gradient.py
@@ -2,18 +2,19 @@ import numpy as np
 
 
 def gradient(x, y, theta):
-    """Computes a gradient vector from three non-empty numpy.ndarray, without any for loop. The
-    ,→ three arrays must have compatible dimensions.
+    """
+    Computes a gradient vector from three non-empty numpy.ndarray,
+    without any for loop. The three arrays must have compatible dimensions.
     Args:
-    x: has to be a numpy.ndarray, a matrix of dimension m * 1.
-    y: has to be a numpy.ndarray, a vector of dimension m * 1.
-    theta: has to be a numpy.ndarray, a 2 * 1 vector.
+        x: has to be a numpy.ndarray, a matrix of dimension m * 1.
+        y: has to be a numpy.ndarray, a vector of dimension m * 1.
+        theta: has to be a numpy.ndarray, a 2 * 1 vector.
     Returns:
-    The gradient as a numpy.ndarray, a vector of dimension 2 * 1.
-    None if x, y, or theta is an empty numpy.ndarray.
-    None if x, y and theta do not have compatible dimensions.
+        The gradient as a numpy.ndarray, a vector of dimension 2 * 1.
+        None if x, y, or theta is an empty numpy.ndarray.
+        None if x, y and theta do not have compatible dimensions.
     Raises:
-    This function should not raise any Exception.
+        This function should not raise any Exception.
     """
     # y_hat = predict_(x, theta)
 
